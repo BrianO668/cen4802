02bf9ece4223c602b681feffa0f1fd915f2db4ca
Brian <oldhamb90@gmail.com>
2026-06-17 14:14:23 -0400

    Removed old iml from local. Int change in main.

--- Assignment1/src/main/java/Main.java
+++ Assignment1/src/main/java/Main.java
@@ -1,14 +1,14 @@
 /**
  * The Main method that is used to find the Fibonacci Sequence term.
  * This method instantiates a FiboFinder class, hard assigns an int to n,
- * and then runs the FiboFinder's FindTerm method to returns the integer that correspons
+ * and then runs the FiboFinder's FindTerm method to returns the integer that corresponds
  * with the nth term of the Fibonacci Sequence.
  */
 public class Main {
     public static void main(String[] args) {
         FiboFinder fibo = new FiboFinder();
 
-        int n = 20;
+        int n = 34;
 
         System.out.println("THIS PROGRAM HAS BEEN MODIFIED SLIGHTLY\n" + "The " + n + "th term of the Fibonacci sequence is: " +
                 fibo.FindTerm(n) + ".");

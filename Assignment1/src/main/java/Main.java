19fa3a3501233119b2c14f0282f96ee304ffdac2
Brian <oldhamb90@gmail.com>
2026-07-09 18:56:19 -0400

    Adding logging. Not yet complete.

--- Assignment1/src/main/java/Main.java
+++ Assignment1/src/main/java/Main.java
@@ -4,13 +4,23 @@
  * and then runs the FiboFinder's FindTerm method to returns the integer that corresponds
  * with the nth term of the Fibonacci Sequence.
  */
+import java.util.logging.Logger;
+
 public class Main {
+    private static final Logger loggy = Logger.getLogger(Main.class.getName());
+
     public static void main(String[] args) {
+        loggy.info("Application starting...");
+
+        loggy.info("FiboFinder instantiating...");
         FiboFinder fibo = new FiboFinder();
+        loggy.info("PrimeFinder instantiating...");
         PrimeFinder pf = new PrimeFinder();
 
+        loggy.info("Assigning value to int n...");
         int n = 34;
 
+        loggy.info("Printing results to console...");
         System.out.println("THIS PROGRAM HAS, ONCE AGAIN, BEEN MODIFIED SLIGHTLY\n" + "The " + n + "th term of the Fibonacci sequence is: " +
                 fibo.FindTerm(n) + ".\nIs this number a prime number?: " + pf.IsPrime(fibo.FindTerm(n)));
     }
